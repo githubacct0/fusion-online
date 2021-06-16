196117834035791c4235d49f75c9e6b8e3e70048
githubacct0 <githubacct0@outlook.com>
2021-06-16 15:46:17 -0400

    remove product list query

--- storefront/src/App.tsx
+++ storefront/src/App.tsx
@@ -1,14 +1,11 @@
 import React from 'react';
 import { BrowserRouter, Switch, Route } from 'react-router-dom';
-import {useAuth, useProductList} from '@saleor/sdk'
 import './App.scss';
 
 import { SearchContainer } from './components/SearchContainer/SearchContainer';
 
 
 function App() {
-  const productList = useProductList({first:10})
-
     return( 
       <BrowserRouter>
         <Switch>
