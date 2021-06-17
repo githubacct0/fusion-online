43d9c22a2bae3d9980da9f5bea96233b6a8eee70
githubacct0 <githubacct0@outlook.com>
2021-06-17 14:12:12 -0400

    widen search filter column

--- storefront/src/components/SearchContainer/SearchContainer.tsx
+++ storefront/src/components/SearchContainer/SearchContainer.tsx
@@ -30,7 +30,7 @@ export const SearchContainer = () => {
   return ( 
     <div className="m-3">
       <Row>
-        <Col xs={1} className="d-flex align-items-center">
+        <Col xs={2} className="d-flex align-items-center">
           <SearchFilters setFilters={(filters: AttributeInput[]) => {setAttributes(filters)}}/>
         </Col>
         <Col>
