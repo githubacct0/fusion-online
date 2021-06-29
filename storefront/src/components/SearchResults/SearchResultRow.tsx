a9aca1f8d0c79c3c5a6d6da3bb1e41bcbdc5e9e6
githubacct0 <githubacct0@outlook.com>
2021-06-29 11:58:59 -0400

    adds select quantity modal

--- storefront/src/components/SearchResults/SearchResultRow.tsx
+++ storefront/src/components/SearchResults/SearchResultRow.tsx
@@ -1,7 +1,7 @@
-import React from 'react';
-import { Button } from 'react-bootstrap';
+import React, { useState } from 'react';
+import { Button, Modal, Table, Form } from 'react-bootstrap';
 import { FontAwesomeIcon } from '@fortawesome/react-fontawesome';
-import { faBookmark as farFaBookmark } from '@fortawesome/pro-regular-svg-icons';
+import { faBookmark as farFaBookmark, faShoppingCart } from '@fortawesome/pro-regular-svg-icons';
 import { faBookmark as fasFaBookmark } from '@fortawesome/pro-solid-svg-icons';
 
 import { Product } from '../../generated/graphql';
@@ -26,6 +26,11 @@ export const SearchResultRow: React.FC<SearchResultRowProps> = ({ otherData: {
     pricing
   }
 }) => {
+  const [show, setShow] = useState(false);
+
+  const handleClose = () => setShow(false);
+  const handleShow = () => setShow(true);
+
   function getAttributeValue (slugName: string): any {
     const matchingAttribute = attributes.filter(
       ({attribute: {slug}}) => slug === slugName)
@@ -50,9 +55,57 @@ export const SearchResultRow: React.FC<SearchResultRowProps> = ({ otherData: {
     <td className="text-center">{variants && variants[0]?.quantityAvailable}</td>
     <td className="text-center">${pricing?.priceRangeUndiscounted?.start?.gross.amount}</td>
     <td className="text-center">
-      <Button variant="primary">
+      <Button variant="primary" onClick={handleShow}>
         Select Quantity
       </Button>
+
+      <Modal
+        show={show}
+        onHide={handleClose}
+        size="md"
+        centered
+      >
+        <Form>
+          <Modal.Header>
+            <Modal.Title className="mb-0">Select Quantity to Order</Modal.Title>
+          </Modal.Header>
+          <Modal.Body>
+            <Table borderless className="mb-0">
+              <thead>
+                <tr>
+                  <th>Available Quantity</th>
+                  <th>Quantity<span className="text-danger">*</span></th>
+                  <th>Unit Price</th>
+                  <th>Total</th>
+                </tr>
+              </thead>
+              <tbody>
+                <tr>
+                  <td>10,000</td>
+                  <td>
+                    <Form.Group controlId="quantity">
+                      <Form.Label className="sr-only">Quantity</Form.Label>
+                      <Form.Control type="number" style={{'maxWidth' : '80px'}} required />
+                    </Form.Group>
+                  </td>
+                  <td>$000.00</td>
+                  <td>$000.00</td>
+                </tr>
+              </tbody>
+            </Table>
+
+            <em>Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua.</em>
+          </Modal.Body>
+          <Modal.Footer>
+            <Button type="submit" variant="primary" onClick={handleClose}>
+              Add to Order <FontAwesomeIcon icon={faShoppingCart} />
+            </Button>
+            <Button variant="link" onClick={handleClose}>
+              Cancel
+            </Button>
+          </Modal.Footer>
+        </Form>
+      </Modal>
     </td>
   </tr>)
 }
