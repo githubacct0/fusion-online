aa52b9dc8b40eda57401d01e7476ab26bae906c1
githubacct0 <githubacct0@outlook.com>
2021-07-07 14:38:37 -0400

    offer model and migration

--- saleor/saleor/fusion_online/api/offer/serializers.py
+++ saleor/saleor/fusion_online/api/offer/serializers.py
@@ -1,5 +1,5 @@
 from rest_framework import serializers
-
+from ..offer.models import Offer
 class OfferSerializer(serializers.Serializer):
 	type = serializers.CharField()
 	date_added = serializers.IntegerField(max_value=None, min_value=None)
@@ -18,4 +18,14 @@ class OfferSerializer(serializers.Serializer):
 	vendor_type = serializers.CharField()
 	vendor_region = serializers.CharField()  
 
-
+	def create(self, validated_data):
+		offer_data = {
+			"source": validated_data["source"],
+			"company": validated_data["company"],
+			"item_num_id": validated_data["item_num_id"],
+			"mpn": validated_data["mpn"],
+			"qty": validated_data["qty"],
+			"offer_price": validated_data["offer_price"],
+			"lead_time_days": validated_data["lead_time_days"]
+		}
+		return Offer.objects.create(**offer_data)
--- saleor/saleor/fusion_online/api/offer/views.py
+++ saleor/saleor/fusion_online/api/offer/views.py
@@ -3,6 +3,7 @@ from rest_framework.decorators import api_view
 from rest_framework.response import Response
 from rest_framework.parsers import JSONParser
 from .serializers import OfferSerializer
+from .models import Offer
 
 @api_view(['POST'])
 def handler(request):
@@ -14,6 +15,7 @@ def handler(request):
 		if not serializer.is_valid():
 			return JsonResponse(serializer.errors, status=400)
 		else:
-			return Response({"fo": "offer submitted"})
+			result = serializer.save()
+			return Response({"fo_ref_id": result.pk})
 	except Exception as e:
 		return Response({"error": True, "message": str(e)}, status=500)
